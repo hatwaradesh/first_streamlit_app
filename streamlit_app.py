a6b0cb4c6acbd8418c5c36c8e1b0841c7908778a
hatwaradesh <135728381+hatwaradesh@users.noreply.github.com>
2023-06-07 11:28:28 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 
 streamlit.title('My Parents new healthy diner')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Masala Dosa, Idli,Vada,Poori')
+streamlit.text('Rava Idli,Bonda Soup')
